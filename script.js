8edcbfdb496158f5a486ca9018edc5ea7a187889
KABAN9KA <93037269+KABAN9KA@users.noreply.github.com>
2023-05-03 15:58:28 +0300

    изменил добавление и редактирвоание

--- script.js
+++ script.js
@@ -1,3 +1,61 @@
+// class Table {
+//
+//     constructor(data) {
+//         this.data = data;
+//         this.table = document.createElement("table");
+//         this.tableBody = document.createElement("tbody");
+//         this.table.appendChild(this.tableBody);
+//         this.addRows();
+//     }
+//     generateTable() {
+//         let table = document.createElement("table");
+//         for (let row of this.data) {
+//             let tr = document.createElement("tr");
+//             for (let cell of row) {
+//                 let td = document.createElement("td")
+//                 let text = document.createTextNode(cell);
+//                 td.appendChild(text);
+//                 tr.appendChild(td);
+//             }
+//             table.appendChild(tr);
+//             table.setAttribute("id", "table");
+//             table.setAttribute("contenteditable", "true");
+//         }
+//         return table;
+//     }
+//     addRows() {
+//         let btn = document.createElement('button');//создаём нашу кнопку
+//         let textInBtn = document.createTextNode('add');//создаем текст для кнопки
+//         btn.appendChild(textInBtn);//добавляем текст в кнопку
+//         document.body.appendChild(btn);
+//
+//         btn.onclick = function (){
+//             let table = document.getElementById( 'table' );
+//             let rowCount = table.rows.length;
+//             let row = table.insertRow(rowCount),
+//                 cell1 = row.insertCell(0),
+//                 cell2 = row.insertCell(1),
+//                 cell3 = row.insertCell(2);
+//             cell1.innerHTML = 'Номер записи';
+//             cell2.innerHTML = 'Заголовок';
+//             cell3.innerHTML = 'Описание';
+//
+//
+//         };
+//     }
+// }
+//
+// let data = [
+//     ["Name", "Age", "Gender"],
+//     ["Да", "1", "М"],
+//     ["Нет", "2", "Ж"],
+// ];
+// let myTable = new Table(data);
+// document.body.appendChild(myTable.generateTable());
+
+
+
+
 class Table {
 
     constructor(data) {
@@ -19,6 +77,7 @@ class Table {
             }
             table.appendChild(tr);
             table.setAttribute("id", "table");
+table.setAttribute("contenteditable", "true");
         }
         return table;
     }
@@ -29,16 +88,17 @@ class Table {
          document.body.appendChild(btn);
 
          btn.onclick = function (){
-             let table = document.getElementById('table');
+             let table = document.getElementById( 'table' );
              let rowCount = table.rows.length;
-             let cellCount = table.rows[0].cells.length;
-             let row = table.insertRow(rowCount);
-             for (let i = 0; i <= cellCount; i++) {
-                 let cell = 'cell' + i;
-                 cell = row.insertCell(i);
-                 let copycel = document.getElementById('col' + i).innerHTML;
-                 cell.innerHTML = copycel;
-             }
+             let row = table.insertRow(rowCount),
+                 cell1 = row.insertCell(0),
+                 cell2 = row.insertCell(1),
+                 cell3 = row.insertCell(2);
+             cell1.innerHTML = '';
+             cell2.innerHTML = '';
+             cell3.innerHTML = '';
+
+
         };
     }
 
